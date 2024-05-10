133766b9f6be5840e884a77ce523b2d8c47e91ee
Thomas-Rafferty Leahy <78023315+trleahy@users.noreply.github.com>
2024-05-10 14:07:22 +0100

    202405.1
    Added Personal Portfolio V2.

---
+++ sites/personal-portfolio-v2/js/ArchiveButton.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Find the button element
+    var floatingButton = document.getElementById("floatingButton");
+
+    // Add a click event listener to the button
+    floatingButton.addEventListener("click", function() {
+        // Navigate to the root of the website
+        window.location.href = "https://trleahy.github.io/archive";
+    });
+});
